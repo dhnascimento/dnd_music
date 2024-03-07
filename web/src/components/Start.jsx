44c74866193988705d6fd83750d80bb7cf981079
danihermes <danielhn@gmail.com>
2024-03-07 13:31:48 -0500

    migrate front-end from CRA to Vite

--- web/src/components/Start.jsx
+++ web/src/components/Start.jsx
@@ -3,13 +3,34 @@ import { loginUrl } from "../helpers/authUrl";
 
 export default function Start({ code, userData }) {
 
+  const handleClick = async () => {
+    try {
+      const request = await fetch("http://localhost:8000/character", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ ...userData }),
+      });
+      const data = await request.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
 
+  }
 
   return (
     <React.Fragment>
         {code && <><p>We have a code!</p></>}
         {!code &&  <a href={loginUrl}>Start Authorization</a>}
-        {userData && <h3>User data received!</h3>}
+        {userData && 
+          <>
+          <h3>User data received!</h3>
+          <button onClick={handleClick}>Send Prompt</button>
+          </>
+        }
+
     </React.Fragment>
   );
 }
