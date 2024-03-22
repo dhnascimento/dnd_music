b9271856d1f5f6d1136e3b88f72e11cea65f8fb3
dhnascimento <danielhn@gmail.com>
2024-03-22 13:12:22 -0400

    mock data to test more than 2 "stages"

--- web/src/helpers/stages.js
+++ web/src/helpers/stages.js
@@ -1,6 +1,6 @@
 const stages = [
     {
-        paragraphs:[
+        paragraphs: [
             'Welcome to the Magic Beats Dungeon!',
             'Here you will go through a journey of self-discovery to find out more about your inner hero.',
             'Share your inner music with the Beats Oracle and we shall what lies ahead. When you are ready, press "Start" below and let us begin!'
@@ -9,13 +9,44 @@ const stages = [
         cta: 'Start'
     },
     {
-        paragraphs:[
+        paragraphs: [
             'Your first challenge lies ahead.',
             'Get rid of what is blocking your path. The Beat Oracle is waiting right around the corner.',
             'Press the "Move forward" button when you are ready.',
         ],
-        title: 'Stage 1: Warm-up',
-        cta: 'Move Forward'
+        title: 'Stage 1: Warm-up!!',
+        cta: 'Move Forward',
+        setCode: false
+    },
+    {
+        paragraphs: [
+            'Excelent, one less hurdle to overcome.',
+            'I regret to say this, but I lied.',
+            'The Beat Oracle is actually around that corner you now can see. So just keep moving and you will get there. No more surprises(?).',
+        ],
+        title: 'Stage 2: Ops..',
+        cta: 'Move Forward Again',
+    },
+    {
+        paragraphs: [
+            'This is getting more consistent.',
+            'Seems like I can build up on the existing narrative and begin to do some fun stuff.',
+            'Shall I add more lines?',
+            'I am definitely adding more lines.',
+        ],
+        title: 'Stage 3: Lala',
+        cta: 'Gogogo',
+    },
+    {
+        paragraphs: [
+            'This is the end.',
+            'My only friend, the end.',
+            '...',
+            '.....',
+            '.....!!!!!',
+        ],
+        title: 'Stage 4: YoHo!',
+        cta: 'End',
     },
 ];
 
